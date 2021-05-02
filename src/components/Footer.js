24b75d3819000cf1c6bb761d0e69028d5353dc8c
Richard Durazo <rcdurazo@gmail.com>
2021-05-02 15:21:10 -0700

    syntax fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,27 +5,27 @@ const Footer = () => {
         <footer className="content-footer flex-column center-items" id="contact">
             <h3>Contact.</h3>
             <div>
-                <ul class='icons'>
+                <ul className='icons'>
                     <li>
                         <a href='https://github.com/richdurazo' target='_blank'>
-                            <i class='fa fa-github fa-3x'></i>
+                            <i className='fa fa-github fa-3x'></i>
                         </a>
                     </li>
                     <li>
                         <a
                             href='https://www.linkedin.com/in/rdurazo'
                             target='_blank'>
-                            <i class='fa fa-linkedin fa-3x'></i>
+                            <i className='fa fa-linkedin fa-3x'></i>
                         </a>
                     </li>
                     <li>
                         <a href='mailto:rcdurazo@gmail.com'>
-                            <i class='fa fa-envelope fa-3x'></i>
+                            <i className='fa fa-envelope fa-3x'></i>
                         </a>
                     </li>
                     <li>
                         <a href='tel:951-317-4973'>
-                            <i class='fa fa-phone fa-3x'></i>
+                            <i className='fa fa-phone fa-3x'></i>
                         </a>
                     </li>
                 </ul>
